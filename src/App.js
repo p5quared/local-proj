0fc3dc85171290d9c9a18cb491b6bf87783b25cb
p5quared <petervaiciulis@gmail.com>
2022-04-23 14:20:55 -0600

    clearing src control

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import sketch from "../public/sketched-facade.jpeg"
 
 function App() {
   return (
     <div className="home-wrapper">
       <Header />
-      <div className='rect-placeholder'></div>
+      <div className='rect-placeholder'>
+        <img src={sketch} alt="Sketch of blue moon cafe"/>
+      </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
